8a9fc55bfc0bf6d85a65f37ac043a7323db3b9a5
yingsm <yingshuming@gmail.com>
2024-11-26 10:23:43 +0800

    封装高评分房源数据请求

--- src/services/modules/home.js
+++ src/services/modules/home.js
@@ -1,7 +1,15 @@
 import YRequest from '../request';
 
+// 获取高性价比房源数据
 export async function getHomeGoodPriceData() {
   return YRequest.get({
     url: '/home/goodprice',
+  });
+}
+
+// 获取高评分房源数据
+export async function getHomeHighScoreData() {
+  return YRequest.get({
+    url: '/home/highscore',
   });
 }
