7a6dcfdb0053583110cda15b7a75f18e1351e29b
yingsm <yingshuming@gmail.com>
2024-11-27 10:45:24 +0800

    entire 页数据渲染

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,7 +1,26 @@
 import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
+import RoomItem from '@/components/room-item';
 import { EntireRoomsWrapper } from './style';
 const EntireRooms = memo(() => {
-  return <EntireRoomsWrapper>EntireRooms</EntireRoomsWrapper>;
+  const { roomList, totalCount } = useSelector(state => ({
+    roomList: state.entire.roomList,
+    totalCount: state.entire.totalCount,
+  }));
+  return (
+    <EntireRoomsWrapper>
+      {/* 标题 */}
+      <h2 className='title'>
+        共 <em>{totalCount}</em> 处住所
+      </h2>
+      {/* 房间列表 */}
+      <div className='list'>
+        {roomList.map(item => {
+          return <RoomItem key={item.id} itemData={item} itemWidth='25%' />;
+        })}
+      </div>
+    </EntireRoomsWrapper>
+  );
 });
 
 export default EntireRooms;
--- src/views/entire/c-cpns/entire-rooms/style.js
+++ src/views/entire/c-cpns/entire-rooms/style.js
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const EntireRoomsWrapper = styled.div``;
+export const EntireRoomsWrapper = styled.div`
+  padding: 40px 20px;
+  margin: 0 -8px;
+
+  .title {
+    margin: 0 0 10px 10px;
+    font-size: 22px;
+    font-weight: 700;
+    color: #222;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
