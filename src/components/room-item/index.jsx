bc2f1666ac95d4747cf3d461c3bc70fbf783a235
yingsm <yingshuming@gmail.com>
2024-11-27 16:24:35 +0800

    房间图片展示集成轮播图

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,29 +1,66 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useRef } from 'react';
 import { ItemWrapper } from './style';
 import { Rating } from '@mui/material';
-const RoomItem = memo(props => {
-  const { itemData, itemWidth = '25%' } = props;
+import { Carousel } from 'antd';
+import IconArrowLeft from '@/assets/svg/icon-arrow-left';
+import IconArrowRight from '@/assets/svg/icon-arrow-right';
+
+const RoomItem = memo(({ itemData, itemWidth = '25%' }) => {
+  const carouselRef = useRef();
+
+  const controlClickHandle = useCallback(isNext => {
+    if (carouselRef.current) {
+      isNext ? carouselRef.current.next() : carouselRef.current.prev();
+    }
+  }, []);
+
   return (
-    <ItemWrapper
-      itemWidth={itemWidth}
-      verifyColor={itemData?.verify_info?.text_color || '#39576a'}
-    >
+    <ItemWrapper itemWidth={itemWidth}>
       <div className='inner'>
-        <div className='cover'>
-          <img src={itemData.picture_url} alt='' />
-        </div>
-        <div className='desc'>{itemData.verify_info.messages.join('·')}</div>
-        <div className='name'>{itemData.name}</div>
-        <div className='price'>￥{itemData.price}/晚</div>
+        {!itemData?.picture_urls ? (
+          <div className='cover'>
+            <img src={itemData?.picture_url} alt='' />
+          </div>
+        ) : (
+          <div className='slider'>
+            <div className='control'>
+              <div
+                className='btn left'
+                onClick={e => controlClickHandle(false)}
+              >
+                <IconArrowLeft width='24' height='24' />
+              </div>
+              <div
+                className='btn right'
+                onClick={e => controlClickHandle(true)}
+              >
+                <IconArrowRight width='24' height='24' />
+              </div>
+            </div>
+            {/* 轮播图 */}
+            <Carousel dots={false} ref={carouselRef}>
+              {itemData?.picture_urls?.map(url => {
+                return (
+                  <div className='cover' key={url}>
+                    <img src={url} className='item' alt='' />
+                  </div>
+                );
+              })}
+            </Carousel>
+          </div>
+        )}
+        <div className='desc'>{itemData?.verify_info?.messages?.join('·')}</div>
+        <div className='name'>{itemData?.name}</div>
+        <div className='price'>￥{itemData?.price}/晚</div>
         <div className='bottom'>
           <Rating
             name='half-rating-read'
-            value={itemData.star_rating ?? 5}
+            value={itemData?.star_rating ?? 5}
             precision={0.5}
             sx={{ fontSize: '12px', color: '#00848A' }}
             readOnly
           />
-          <span className='reviews'>{itemData.reviews_count}条评价</span>
+          <span className='reviews'>{itemData?.reviews_count}条评价</span>
           {itemData?.bottom_info?.content && (
             <span className='extra'>·{itemData?.bottom_info?.content}</span>
           )}
