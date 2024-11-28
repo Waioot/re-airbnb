e16242837f75bd57baec5b6aecc886104debe8fb
yingsm <yingshuming@gmail.com>
2024-11-28 19:53:10 +0800

    引入underscore的throttle进行节流

--- src/hooks/useScrollPosition.js
+++ src/hooks/useScrollPosition.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
+import { throttle } from 'underscore';
 
 export default function useScrollPosition() {
   const [scrollX, setScrollX] = useState(0);
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
-    const updatePosition = () => {
+    // 引入节流
+    const updatePosition = throttle(() => {
       setScrollX(window.scrollX);
       setScrollY(window.scrollY);
-    };
+    }, 100);
     window.addEventListener('scroll', updatePosition);
     return () => {
       window.removeEventListener('scroll', updatePosition);
