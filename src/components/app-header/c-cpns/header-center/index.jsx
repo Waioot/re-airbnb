51aef495d608b47869754af0927ff4e4eef076ec
yingsm <yingshuming@gmail.com>
2024-11-28 19:43:35 +0800

    完善顶部搜索按钮动画

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,5 +1,6 @@
 import React, { memo, useState } from 'react';
 import { CenterWrapper } from './style';
+import { CSSTransition } from 'react-transition-group';
 
 import IconSearchBar from '@/assets/svg/icon_search_bar';
 import SearchTabs from './c-cpns/saerch-tabs';
@@ -17,27 +18,35 @@ const HeaderCenter = memo(({ isSearch, searchClick }) => {
   }
   return (
     <CenterWrapper>
-      {isSearch ? (
-        <>
-          <div className='search-detail'>
-            <SearchTabs titles={titles} tabClick={tabClickHandle} />
-            <div className='infos'>
-              <SearchSections
-                searchInfos={SearchTitles[currentIndex].searchInfos}
-              />
-            </div>
+      <CSSTransition
+        in={!isSearch}
+        timeout={250}
+        classNames='bar'
+        unmountOnExit={true}
+      >
+        <div className='search-bar' onClick={searchClickHandle}>
+          <div className='text'>搜索房源和体验</div>
+          <div className='icon'>
+            <IconSearchBar />
           </div>
-        </>
-      ) : (
-        <>
-          <div className='search-bar' onClick={searchClickHandle}>
-            <div className='text'>搜索房源和体验</div>
-            <div className='icon'>
-              <IconSearchBar />
-            </div>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={isSearch}
+        timeout={250}
+        classNames='detail'
+        unmountOnExit={true}
+      >
+        <div className='search-detail'>
+          <SearchTabs titles={titles} tabClick={tabClickHandle} />
+          <div className='infos'>
+            <SearchSections
+              searchInfos={SearchTitles[currentIndex].searchInfos}
+            />
           </div>
-        </>
-      )}
+        </div>
+      </CSSTransition>
     </CenterWrapper>
   );
 });
