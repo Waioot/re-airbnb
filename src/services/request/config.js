f818d880104ee991284682a96abc6c47ea6abfe6
yingsm <yingshuming@gmail.com>
2024-12-11 19:26:06 +0800

    添加 Vercel 代理转发，避免Mixed Content: The page at '<URL>' was loaded over HTTPS 错误

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://codercba.com:1888/airbnb/api';
+// 开发环境还是vercel 环境
+const VERCEL = process.env.REACT_APP_VERCEL;
+const BASE_URL = VERCEL === 'true' ? '/api' : process.env.REACT_APP_BASE_URL;
 const TIMEOUT = 10000;
 
 export { BASE_URL, TIMEOUT };
